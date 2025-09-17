76929b1f22b28fb04a928d5e715aea32554e0209
Zhafrnn <haydarzhafran3@gmail.com>
2025-09-17 14:27:25 +0700

    Belajar tipe data

---
+++ 3_type_data.dart
@@ -0,0 +1,58 @@
+/**
+ * Tipe data adalah suatu konsep yang digunakan
+ * untuk mengklasifikasikan janis data
+ * yang dapat diproses oleh program
+ */
+
+/**
+ * Jenis tipe data
+ * 
+ * int = interger : bilangan bulat
+ * 1, -2, 11, dsb
+ */
+
+/**
+ * double = bilangan desimal
+ * 3.14, -0.1, 2.5, dsb
+ */
+
+/**
+ * num = gabungan dari interger dan double
+ * 3.14, 2, -2.0, 3.15
+ */
+
+/**
+ * bool = boolean : true or false
+ */
+
+/**
+ * String = string : "kumpulan karakter"
+ */
+
+/**
+ * List = berfupa daftar
+ * [1,2,3,4,5,6,7,8,9,0,-1,-2,-3]
+ * ['a','b','c', 'd']
+ */
+
+/**
+ * Map = berupa key:value
+ * {nama_kota : "Jakarta", "ID" : 62}
+ */
+
+/**
+ * Dynamic = campuran / tipe apapun
+ */
+
+void main(List<String> args) {
+  String nama = "Haydar Zhafran";
+  int age = 15;
+
+  var tinggi_badan;
+  tinggi_badan = 165;
+  tinggi_badan = 'Seratus Enam Puluh Lima';
+
+  bool isMarried = false;
+  double berat_badan = 60;
+  
+}
--- main.dart
+++ main.dart
@@ -1,4 +1,4 @@
 void main(List<String> args) {
   print('Hello, World!');
-  
+  print('Al, udah kenyang');
 }
